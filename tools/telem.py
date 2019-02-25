c01201ac0b25ba3aba34b5716ae19601033c6724
Zongtong Luo <luozongtong123@163.com>
2019-02-25 11:05:01 +0800

    add ArduSub v3.6.0-dev support #2

--- tools/telem.py
+++ tools/telem.py
@@ -5,7 +5,9 @@ from time import sleep
 home = os.environ['HOME'] 
 
 while True:
-  while ((os.system("ls /dev/serial/by-id/usb-3D_Robotics_PX4_FMU_v2.x_0-if00 2>/dev/null") != 0) or (os.path.isfile(home+"/companion/scripts/start_mavproxy_telem_splitter.sh")==0)):
+  while (((os.system("ls /dev/serial/by-id/usb-3D_Robotics_PX4_FMU_v2.x_0-if00 2>/dev/null") != 0) and 
+          (os.system("ls /dev/serial/by-id/usb-ArduPilot_Pixhawk1_200042000E51343032383731-if00 2>/dev/null") != 0)) or 
+          (os.path.isfile(home+"/companion/scripts/start_mavproxy_telem_splitter.sh")==0)):
     sleep(2)
 
   os.system(home+"/companion/scripts/start_mavproxy_telem_splitter.sh")
